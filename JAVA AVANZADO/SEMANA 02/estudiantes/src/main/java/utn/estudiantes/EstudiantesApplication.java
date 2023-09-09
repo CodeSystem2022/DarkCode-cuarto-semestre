9d2e6dc4937fa2023c3dd67318b25e45f5fe2476
Javier Brega <80373813+javierbrega@users.noreply.github.com>
2023-09-09 17:03:09 -0300

    Actulizo semana 2

--- JAVA AVANZADO/SEMANA 02/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ JAVA AVANZADO/SEMANA 02/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -3,6 +3,7 @@ package utn.estudiantes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class EstudiantesApplication {
 
